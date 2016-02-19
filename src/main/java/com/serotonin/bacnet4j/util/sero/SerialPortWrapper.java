5f680053864682cba5b1ada2ea00321bba5c32ce
terrypacker <username@users.noreply.github.com>
2016-02-19 06:35:49 -1000

    Changing comments in SerialPortWrapper to include GNU license info for Modbus4J.

--- src/main/java/com/serotonin/bacnet4j/util/sero/SerialPortWrapper.java
+++ src/main/java/com/serotonin/bacnet4j/util/sero/SerialPortWrapper.java
@@ -1,5 +1,29 @@
-/**
+/*
+ * ============================================================================
+ * GNU General Public License
+ * ============================================================================
+ *
  * Copyright (C) 2015 Infinite Automation Software. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * When signing a commercial license with Infinite Automation Software,
+ * the following extension to GPL is made. A special exception to the GPL is
+ * included to allow you to distribute a combined work that includes BAcnet4J
+ * without being obliged to provide the source code for any proprietary components.
+ *
+ * See www.infiniteautomation.com for commercial license options.
+ * 
  * @author Terry Packer
  */
 package com.serotonin.bacnet4j.util.sero;
@@ -8,7 +32,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- * Wrapper to further aid in abstracting Modbus4J from a serial port implementation
+ * Wrapper to further aid in abstracting BACnet4J from a serial port implementation
  * 
  * @author Terry Packer
  *
